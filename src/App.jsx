bdcd449daffdb4fdcfdfea47cc9fadb2a430a6d0
p-mn19 <poornimamen2019@gmail.com>
2025-02-13 19:44:31 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Card = ({ title }) => {
+    return(
+        <div>
+            <h2>{title}</h2>
+        </div>
+    );
+}
 
 const App  = () => {
-  
+    return(
+        <div>
+            <h2>App</h2>
+            <Card title="The lion king"/>
+            <Card title="Interstellar"/>
+            <Card title="Men in black"/>
+        </div>
+    );
 }
 export default App
