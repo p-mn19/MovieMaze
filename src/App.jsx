e740bd820cd5f19b44b50bf830b73256f2e136c1
p-mn19 <poornimamen2019@gmail.com>
2025-05-03 16:21:14 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
-import { useState,useEffect } from "react";
+import React from "react";
+
+const App = () => {
+    return ( 
+        <main>
+            <div classname="pattern"/>
+            <div className="wrapper">
+             <header>
+                <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy, Without the Hassle</h1>
+             </header>
+
+             <p></p>
+            </div>
+        </main>
+     );
+}
+ 
+export default App;
 
 //when you reload the page the component will get re-rendered and the state 
 //will return to its initial value
 //hooks are special functions that allow us to tap into react features like state management
-const Card = ({ title }) => {
-    const [] = useState();
-    const [hasLiked,setHasLiked] = useState(false);
-     useEffect(() => {
-        console.log(`${title} has been liked: ${hasLiked}`);
-     });
-    return(
-        <div className="card">
-            <h2>{title}</h2>
-            <button onClick={() => setHasLiked(!hasLiked)}>
-            {hasLiked ? 'Liked' : 'Like'}
-            </button>
-        </div>
-    );
-}
 
-const App  = () => {
-    return(
-        <div className="card-container">
-            <Card title="The lion king"/>
-            <Card title="Interstellar"/>
-            <Card title="Men in black"/>
-        </div>
-    );
-}
-export default App
