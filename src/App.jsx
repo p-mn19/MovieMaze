5deb08b2582a8bba696ec1c84b9f66116f932639
p-mn19 <poornimamen2019@gmail.com>
2025-06-13 13:06:04 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,14 @@ const App = () => {
     const [movieList, setMovieList] = useState([]);
     const [isLoading,setIsLoading] = useState(false)
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query='') => {
         setIsLoading(true);
         setErrorMessage('');
 
         try{
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query 
+            ? `{API_BASE_URL}/search/movie/query=${encodeURIComponent(query)}`
+            :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
             const response = await fetch(endpoint,API_OPTIONS)
             
@@ -47,8 +49,8 @@ const App = () => {
         }
     }
     useEffect(() => {
-        fetchMovies();
-      }, []);
+        fetchMovies(searchTerm);
+      }, [searchTerm]); //whenever the searchTerm changes fetchMovies will be called thats why we add the dependency array
     
     return ( 
         <main>
